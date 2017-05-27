85bf0047c29a3c3651a81e561352d7052d26a9db
Jason Frey <fryguy9@gmail.com>
2017-05-26 22:41:40 -0400

    Prepare and drop a database for the specs.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,38 @@ RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
+  config.before(:suite) do
+    drop_test_database
+    create_test_database
   end
+
+  config.before do
+    ActiveRecord::Base.connection.begin_transaction
+  end
+
+  config.after do
+    ActiveRecord::Base.connection.rollback_transaction if ActiveRecord::Base.connection.transaction_open?
+  end
+
+  config.after(:suite) do
+    drop_test_database
+  end
+end
+
+DB_NAME = "activerecord_id_regions_test".freeze
+
+def drop_test_database
+  ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database => "postgres")
+  ActiveRecord::Base.connection.drop_database(DB_NAME)
+end
+
+def create_test_database
+  ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database => "postgres")
+  ActiveRecord::Base.connection.create_database(DB_NAME)
+  ActiveRecord::Base.establish_connection(:adapter => "postgresql", :database => DB_NAME)
+  ActiveRecord::Base.connection.create_table("test_records", :id => :bigserial)
+end
+
+class TestRecord < ActiveRecord::Base
+  include ActiveRecord::IdRegions
 end
