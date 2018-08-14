fa865f9c7dbe0c4cf412f0c5c342ff0b2139a7ee
Brandon Dunne <brandondunne@hotmail.com>
2018-08-14 12:51:31 -0400

    Detect the region number before creating the new table
    In the case that the sequence needs to be pulled from an existing table in
    the database, we need to detect the region number before creating a new
    table (and its new sequence).  If we do it after creating the table and its
    sequence, we have a chance of selecting that sequence and using it to
    determine the region number, which would incorrectly be 0.

--- lib/active_record/id_regions/migration.rb
+++ lib/active_record/id_regions/migration.rb
@@ -2,10 +2,10 @@ module ActiveRecord::IdRegions
   module Migration
     def create_table(table_name, options = {})
       options[:id] = :bigserial if options[:id].nil?
+      value = anonymous_class_with_id_regions.rails_sequence_start
       super
       return if options[:id] == false
 
-      value = anonymous_class_with_id_regions.rails_sequence_start
       set_pk_sequence!(table_name, value) unless value == 0
     end
 
